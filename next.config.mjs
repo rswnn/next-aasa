894947120648f56f1a16bd16906c667a8fd43703
riswan ardiansah <riswanardiansah@gmail.com>
2024-06-16 16:46:24 +0700

    add aasa file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [{ key: "content-type", value: "application/json" }]
+      }
+    ];
+  }
+};
 
 export default nextConfig;
